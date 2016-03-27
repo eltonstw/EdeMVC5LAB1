5735f847fe4966b2604a73eb62c0db4e185b6ad9
Elton <eltons@edetw.com>
2016-03-28 01:52:12 +0800

    MVC5 LAB1

---
+++ LAB/Controllers/客戶資料檢視表Controller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using LAB.Models;
+
+namespace LAB.Controllers
+{
+    public class 客戶資料檢視表Controller : Controller
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+
+        // GET: 客戶資料檢視表
+        public ActionResult Index()
+        {
+            return View(db.客戶資料檢視表.Select(c => new CustomerDataViewModel()
+            {
+                客戶名稱 = c.客戶名稱,
+                銀行帳戶數量 = c.銀行帳戶數量,
+                聯絡人數量 = c.聯絡人數量
+            }).ToList());
+        }
+
+        public ActionResult List(string input)
+        {
+            if (string.IsNullOrEmpty(input.Trim()))
+            {
+                return RedirectToAction("Index");
+            }
+
+            var data = db.客戶資料檢視表.Where(c => c.客戶名稱.Contains(input.Trim())).Select(c => new CustomerDataViewModel()
+            {
+                客戶名稱 = c.客戶名稱,
+                銀行帳戶數量 = c.銀行帳戶數量,
+                聯絡人數量 = c.聯絡人數量
+            }).ToList();
+            return View("Index", data);
+        }
+    }
+}
---
+++ LAB/Models/CustomerDataViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LAB.Models
+{
+    public class CustomerDataViewModel
+    {
+        public string 客戶名稱 { get; set; }
+        public int? 聯絡人數量 { get; set; }
+        public int? 銀行帳戶數量 { get; set; }
+    }
+}
